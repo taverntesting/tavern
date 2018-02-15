49448dcaac1f3bcdb54e0594a4cc0166aa680b59
Michael Boulton <boulton@zoetrope.io>
2018-02-15 10:51:53 +0000

    Fix formatting mqtt failures

--- tavern/printer.py
+++ tavern/printer.py
@@ -12,9 +12,9 @@ def log_pass(test, response):
 def log_fail(test, response, expected):
     # TODO print more info
     fmt = "FAILED: {:s} [{}]"
-    if response:
+    try:
         formatted = fmt.format(test["name"], response.status_code)
-    else:
+    except AttributeError:
         formatted = fmt.format(test["name"], "N/A")
     logger.error(formatted)
     logger.error("Expected: %s", expected)
