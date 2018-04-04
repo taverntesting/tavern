72feec6e894c4a0ff15411d996edc413c99e442e
Michael Boulton <boulton@zoetrope.io>
2018-04-04 16:39:27 +0100

    fix port in regex test

--- tests/integration/server.py
+++ tests/integration/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/token", methods=["GET"])
 def token():
-    return '<div><a src="http://127.0.0.1:5000/verify?token=c9bb34ba-131b-11e8-b642-0ed5f89f718b">Link</a></div>', 200
+    return '<div><a src="http://127.0.0.1:5003/verify?token=c9bb34ba-131b-11e8-b642-0ed5f89f718b">Link</a></div>', 200
 
 @app.route("/verify", methods=["GET"])
 def verify():
