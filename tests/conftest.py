40bcd781794bf82b1b0e8b31b4b8ab94e3803f15
Michael Boulton <boulton@zoetrope.io>
2018-07-03 12:53:17 +0100

    Add better names for fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,10 +13,17 @@ def run_all():
 
 
 @pytest.fixture
-def abc():
+def str_fixture():
     return "abc-fixture-value"
 
 
-@pytest.fixture(name="def")
-def sdkofsok(abc):
-    return abc
+@pytest.fixture(name="yield_str_fixture")
+def sdkofsok(str_fixture):
+    yield str_fixture
+
+
+@pytest.fixture(name="yielder")
+def bluerhug():
+    # This doesn't really do anything at the moment. In future it might yield
+    # the result or something, but it's a bit difficult to do at the moment.
+    response = (yield "hello")
