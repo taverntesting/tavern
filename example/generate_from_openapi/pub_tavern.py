9c68ddcfc0f3b09e3ddab4df228db76c74573ec3
Ling Li <ling.li@utiliteam.co.uk>
2019-01-08 22:29:05 +0000

    Change to allow script to run from command line

--- example/generate_from_openapi/pub_tavern.py
+++ example/generate_from_openapi/pub_tavern.py
@@ -1,23 +1,39 @@
 import yaml
 from coreapi import Client
 import openapi_codec
+import sys
 
-client = Client()
-json_path = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/petstore-simple.json"
-d = client.get(json_path, format="openapi")
 
-test_dict = {}
-for test_name in d.links.keys():
-    test_dict["test_name"] = test_name
+def generate_tavern_yaml(json_path):
+    client = Client()
+    d = client.get(json_path, format="openapi")
 
-    request = {
-        "url": d.links[test_name].url,
-        "method": str.upper(d.links[test_name].action),
-    }
+    test_dict = {}
+    for test_name in d.links.keys():
+        test_dict["test_name"] = test_name
 
-    response = {"strict": False, "status_code": 200}
-    inner_dict = {"name": test_name, "request": request, "response": response}
+        request = {
+            "url": d.links[test_name].url,
+            "method": str.upper(d.links[test_name].action),
+        }
 
-    test_dict["stages"] = [inner_dict]
-    print(test_dict)
-    print(yaml.dump(test_dict, explicit_start=True, default_flow_style=False))
+        response = {"strict": False, "status_code": 200}
+        inner_dict = {"name": test_name, "request": request, "response": response}
+
+        test_dict["stages"] = [inner_dict]
+        print(test_dict)
+        print(yaml.dump(test_dict, explicit_start=True, default_flow_style=False))
+
+
+def help():
+    print("pub_tavern.py <url to openapi.json>")
+    print(
+        "eg: pub_tavern.py https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/petstore-simple.json"
+    )
+    exit(-1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        help()
+    generate_tavern_yaml(sys.argv[1])
