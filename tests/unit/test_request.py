f15cbe44499c4ad9beec623bde600b22b108d6c5
Michael Boulton <michaelboulton@gmail.com>
2019-02-03 10:05:18 +0000

    Add more tests + fix old ones

--- tests/unit/test_request.py
+++ tests/unit/test_request.py
@@ -94,12 +94,34 @@ class TestRequestArgs(object):
 
         assert args["method"] == "POST"
 
-    def test_default_content_type(self, req, includes):
+    @pytest.mark.parametrize("extra, ctype", [
+        {}, None,
+        {"json": [1, 2, 3]},
+        {"data": {"a": 2}}
+    ])
+    def test_default_content_type(self, req, includes, extra, ctype):
+        del req["headers"]["Content-Type"]
+
+        includes.update(**extra)
+
+        args = get_request_args(req, includes)
+
+        assert args["headers"].get("content-type") == ctype
+
+    def test_no_set_content_type(self, req, includes):
         del req["headers"]["Content-Type"]
 
         args = get_request_args(req, includes)
 
-        assert args["headers"]["content-type"] == "application/json"
+        with pytest.raises(KeyError):
+            assert args["headers"]["content-type"]
+
+    def test_cannot_send_data_and_json(self, req, includes):
+        includes["json"] = [1, 2, 3]
+        includes["data"] = [1, 2, 3]
+
+        with pytest.raises(exceptions.BadSchemaError):
+            get_request_args(req, includes)
 
     def test_no_override_content_type(self, req, includes):
         req["headers"]["Content-Type"] = "application/x-www-form-urlencoded"
