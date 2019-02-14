86cb5c47690a1e79f864fe960aff75a5b0bca142
Michael Boulton <michaelboulton@gmail.com>
2019-02-14 18:13:51 +0000

    fix check

--- tavern/testutils/pytesthook/file.py
+++ tavern/testutils/pytesthook/file.py
@@ -112,10 +112,10 @@ def _generate_parametrized_test_items(keys, vals_combination):
     logger.debug("Values for this combination: %s", flattened_values)
 
     def u2string(s):
-        if isinstance(s, ustr):
-            return s.encode("utf8")
-        else:
+        if isinstance(s, str):
             return s
+        else:
+            return s.encode("utf8")
 
     flattened_values = list(map(u2string, flattened_values))
 
