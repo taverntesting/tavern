cc1eb7eaefde6728a50e02beba8a03aa2b24fc51
Michiel Vanderlee <mastermikeyboy@gmail.com>
2019-03-16 11:16:05 -0300

    Don't read any comment, including EOL comments

--- tavern/testutils/pytesthook/error.py
+++ tavern/testutils/pytesthook/error.py
@@ -176,8 +176,8 @@ class ReprdError(object):
             """Get lines between start and end mark"""
             with io.open(filename, "r", encoding="utf8") as testfile:
                 for idx, line in enumerate(testfile.readlines()):
-                    if first_line < idx < last_line and not line.lstrip().startswith("#"):
-                        yield line.rstrip()
+                    if first_line < idx < last_line:
+                        yield line.split("#", 1)[0].rstrip()
 
         code_lines = list(read_relevant_lines(self.item.spec.start_mark.name))
 
