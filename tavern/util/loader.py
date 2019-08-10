0c02ef7a8d34502d50e8011fe1a73d59fdce3f6f
Michael Boulton <michael.boulton@gmail.com>
2019-08-10 10:20:35 +0100

    Fix pylint false positive/negative

--- tavern/util/loader.py
+++ tavern/util/loader.py
@@ -339,7 +339,8 @@ def load_single_document_yaml(filename):
     return contents
 
 
-def error_on_empty_scalar(self, mark):  # pylint: disable=unused-argument
+# 'self' is an unused arguments but pyqlint doesn't complain
+def error_on_empty_scalar(self, mark):
     location = "{mark.name:s}:{mark.line:d} - column {mark.column:d}".format(mark=mark)
     error = "Error at {} - cannot define an empty value in test - either give it a value or explicitly set it to None".format(
         location
