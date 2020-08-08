0a1e729a3a39ffc848d42513d1b908e3fddee510
Michael Boulton <michaelboulton@gmail.com>
2020-08-08 13:50:01 +0100

    change versions
    Might just remove pylint to avoid annoying issues like this

--- setup.py
+++ setup.py
@@ -1,74 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import Command
-import os
-from subprocess import check_call
-
 from setuptools import setup
 
-
-class BuildDocs(Command):
-    description = "Build documentation html"
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        self.execute(check_call,
-            [["sphinx-apidoc", "tavern", "-o", "docs"]],
-            msg="Generating rst for tavern")
-
-        self.execute(check_call,
-            [["make", "-C", "docs", "html"]],
-            msg="Making html docs")
-
-
-class DeployPypi(Command):
-    description = "Deploy to pypi using twine"
-    user_options = [
-        ("repository=", "r", "Repository to upload to"),
-    ]
-
-    def initialize_options(self):
-        self.repository = None  # pylint: disable=attribute-defined-outside-init
-
-    def finalize_options(self):
-        if not self.repository:
-            raise RuntimeError("Need a repository to upload to! Run 'python setup.py upload_twine' for options.")
-
-    def run(self):
-        self.run_command("clean")
-        self.run_command("sdist")
-
-        if len(os.listdir("dist")) > 1:
-            raise RuntimeError("More than one package in dist/ - only one can be present to upload! Delete the dist/ folder before running this command.")
-
-        to_upload = os.path.join("dist", os.listdir("dist")[0])
-
-        args = ["twine", "upload", "-r", self.repository, to_upload]
-
-        self.execute(check_call,
-            [args],
-            msg="Uploading package to pypi")
-
-
 TESTS_REQUIRE = [
-    "pytest-cov==2.8.1",
-    "colorlog==4.1.0",
-    "faker==3.0.0"
+    "pytest-cov",
+    "colorlog",
+    "faker",
+    "prospector[with_pyroma,with_mypy]>=1.3.0,<2",
+    "pygments",
+    "pylint>=2.5.3,<3",
+    "black",
+    "mypy",
+    "mypy-extensions",
+    "isort<5"
 ]
 
 setup(
     name="tavern",
 
-    cmdclass={
-        "docs": BuildDocs,
-        "upload_twine": DeployPypi,
-    },
     tests_require=TESTS_REQUIRE,
     extras_require={
         "tests": TESTS_REQUIRE
