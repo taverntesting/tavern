069080e633d7c45665aca5db1db2464aa9701968
Michael Boulton <michaelboulton@gmail.com>
2020-08-26 16:10:46 +0100

    Add mqtt coverage

--- coverage.sh
+++ coverage.sh
@@ -3,7 +3,8 @@
 set -ex
 
 tox -c tox-integration.ini -e py38-generic
+tox -c tox-integration.ini -e py38-mqtt
 tox -e py38
 
-coverage combine --append .coverage tests/integration/.coverage
+coverage combine --append .coverage tests/integration/.coverage example/mqtt/.coverage
 coverage report -m
