530d411f4ff2d506f132463d45ee2d6631b7b6bf
Michael Boulton <michaelboulton@gmail.com>
2020-12-19 15:08:19 +0000

    Add python multitests

---
+++ pants-plugins/macros.py
@@ -0,0 +1,26 @@
+def python3_multitests(**kwargs):
+    kwargs.pop("interpreter_constraints", None)
+
+    python_tests(
+        name=f"tests_py3.6",
+        interpreter_constraints={"CPython": ">=3.6,<3.7"},
+        **kwargs,
+    )
+
+    python_tests(
+        name=f"tests_py3.7",
+        interpreter_constraints={"CPython": ">=3.7,<3.8"},
+        **kwargs,
+    )
+
+    python_tests(
+        name=f"tests_py3.8",
+        interpreter_constraints={"CPython": ">=3.8,<3.9"},
+        **kwargs,
+    )
+
+    python_tests(
+        name=f"tests_py3.9",
+        interpreter_constraints={"CPython": ">=3.9,<3.10"},
+        **kwargs,
+    )
