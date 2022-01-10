9b480579e6b0d1c0acc97f0efdc6b32ffd3b52cb
Ryan Friedman <ryan_friedman@trimble.com>
2022-01-09 20:44:21 -0600

    Bind all MQTT client callbacks (#753)
    Signed-off-by: Ryan Friedman <ryan_friedman@trimble.com>

--- tavern/_plugins/mqtt/client.py
+++ tavern/_plugins/mqtt/client.py
@@ -153,6 +153,11 @@ class MQTTClient:
             self._client.username_pw_set(**self._auth_args)
 
         self._client.on_message = self._on_message
+        self._client.on_connect = self._on_connect
+        self._client.on_disconnect = self._on_disconnect
+        self._client.on_connect_fail = self._on_connect_fail
+        self._client.on_socket_open = self._on_socket_open
+        self._client.on_socket_close = self._on_socket_close
 
         if self._tls_args:
             try:
@@ -246,6 +251,11 @@ class MQTTClient:
             self._message_queue.put_nowait(message)
         except Full:
             logger.exception("message queue full")
+
+    @staticmethod
+    def _on_socket_close(client, userdata, socket):
+        # pylint: disable=unused-argument
+        logger.debug("MQTT socket closed")
 
     def message_received(self, timeout=1):
         """Check that a message is in the message queue
