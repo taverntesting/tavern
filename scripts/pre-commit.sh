2f987e428dccaffbf9c9ccff0feae34236d150c2
Michael Boulton <michaelboulton@gmail.com>
2022-04-17 15:44:05 +0100

    Add extra deps + fix gazelle

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+bazel run //:gazelle_python_manifest.update
 bazel run //:gazelle
 bazel run --run_under "cd $PWD && " @bazel_buildtools//buildozer \
   'substitute deps @tavern_pip//pypi__([^/]+) @tavern_pip_${1}//:pkg' //...:*
