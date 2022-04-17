58340add0be313590af30c6a01310dd292d4474e
Michael Boulton <michaelboulton@gmail.com>
2022-04-17 13:00:36 +0100

    add pre commit script

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+bazel run //:gazelle
+bazel run --run_under "cd $PWD && " @bazel_buildtools//buildozer \
+  'substitute deps @tavern_pip//pypi__([^/]+) @tavern_pip_${1}//:pkg' //...:*
