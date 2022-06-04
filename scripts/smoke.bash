79e2f50fe41cac2a198680b02ad6ffef2ff61a4e
Michael Boulton <michaelboulton@gmail.com>
2022-06-04 14:52:56 +0100

    Fix mypy check

--- scripts/smoke.bash
+++ scripts/smoke.bash
@@ -9,7 +9,8 @@ tox -c tox.ini        \
   -e py${PYVER}       \
   -e py${PYVER}-pytest6       \
   -e py${PYVER}black  \
-  -e py${PYVER}lint
+  -e py${PYVER}lint   \
+  -e py${PYVER}mypy
 
 tox -c tox-integration.ini  \
   -e py${PYVER}-generic     \
