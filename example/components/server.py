df905c0ee7034adb9585c40f0108fe4cc59c7223
Michael Boulton <michaelboulton@gmail.com>
2022-06-05 15:06:54 +0100

    Fix tests

--- example/components/server.py
+++ example/components/server.py
@@ -26,7 +26,7 @@ def login():
         "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
     }
 
-    token = jwt.encode(payload, SECRET, algorithm="HS256").decode("utf8")
+    token = jwt.encode(payload, SECRET, algorithm="HS256")
 
     return jsonify({"token": token})
 
