62e3802ac8af533eec4c0a98f7d6b3ea7932c5a4
Michael Boulton <michaelboulton@gmail.com>
2022-06-05 13:57:17 +0100

    Stop trying to deploy as it doesn't work

---
+++ scripts/build-bazel-images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+bazel build $(bazel query 'kind("(.*_image|_app_layer)", //...)' )
+
+bazel run :skaffold -- build
--- scripts/cleanup-skaffold.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-command -v parallel
-
-docker ps -qa --filter 'name=tavern-.*' | parallel 'docker rm -f {}'
-docker network ls -q --filter 'name=skaffold-network-.*' | parallel 'docker network rm {}'
