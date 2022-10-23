cfb634c3e07ecd428b62662c7bd518da6f5d28b6
Michael Boulton <michaelboulton@gmail.com>
2022-10-23 11:59:14 +0100

    Remove pointless import

--- bazel/integration_main.py
+++ bazel/integration_main.py
@@ -4,16 +4,12 @@ import subprocess
 
 import pytest
 import sys
-# gazelle:ignore rules_python.python.runfiles
-from rules_python.python.runfiles import runfiles
 
 from tavern._core.internal.testutil import enable_default_tavern_extensions
 from tavern._core.pytest import hooks as pytesthook
 
 if __name__ == "__main__":
     enable_default_tavern_extensions()
-
-    runfiles = runfiles.Create()
 
     test_file_location_ = os.environ["TAVERN_TEST_FILE_LOCATION"]
     os_path_dirname = os.path.dirname(test_file_location_)
