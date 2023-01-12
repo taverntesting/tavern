19f5c42929ef311b985df96479749735cf28bf47
Michael Boulton <michaelboulton@gmail.com>
2023-01-12 15:21:37 +0000

    Update changelog script

--- scripts/update-changelog.bash
+++ scripts/update-changelog.bash
@@ -4,7 +4,7 @@ print_changelog() {
   echo "# Changelog"
   echo
 
-  for current_tag in $(git tag --sort=creatordate | grep -v '^2'); do
+  for current_tag in $(git tag --sort=creatordate | grep -v '2.0.0.*\(a\|b\|rc\)'); do
     tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${current_tag})
 
     hashes="##"
@@ -18,26 +18,6 @@ print_changelog() {
       echo
       cat <<EOF | sed 's/^\s*//g'
 ${tag_content}
-EOF
-    fi
-
-    echo
-  done
-
-  for current_tag in $(git tag --sort=creatordate | grep '^2'); do
-    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${current_tag})
-
-    hashes="##"
-    if [[ "$current_tag" =~ .*0$ ]]; then
-      hashes="#"
-    fi
-    echo "$hashes $() $(git tag -n1 $current_tag) (${tag_date})"
-
-    tag_content=$(git tag -n9 $current_tag | tail -n '+2')
-    if [ -n "$tag_content" ]; then
-      echo
-      cat <<EOF | sed 's/^\s*//g'
-  ${tag_content}
 EOF
     fi
 
