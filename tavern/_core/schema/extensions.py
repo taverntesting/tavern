225be839b5ae7969271afc3229c58719580f9e37
michaelboulton <michaelboulton@users.noreply.github.com>
2023-06-04 12:18:39 +0100

    Update dependencies (#872)

--- tavern/_core/schema/extensions.py
+++ tavern/_core/schema/extensions.py
@@ -138,7 +138,7 @@ def verify_oneof_id_name(value, rule_obj, path) -> bool:
 
     name = value.get("name")
     if not name:
-        if name == "":
+        if name == "":  # noqa
             raise BadSchemaError("Name cannot be empty")
 
         if not value.get("id"):
