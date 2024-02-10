3a8395a432cefca0fca7d77826583dad6d9711e1
michaelboulton <michaelboulton@users.noreply.github.com>
2024-02-10 18:27:52 +0000

    Fix saving in grpc (#916)

--- tavern/_plugins/grpc/response.py
+++ tavern/_plugins/grpc/response.py
@@ -49,7 +49,7 @@ class GRPCResponse(BaseResponse):
         expected: Union[_GRPCExpected, Mapping],
         test_block_config: TestConfig,
     ) -> None:
-        check_expected_keys({"body", "status", "details"}, expected)
+        check_expected_keys({"body", "status", "details", "save"}, expected)
         super().__init__(name, expected, test_block_config)
 
         self._client = client
