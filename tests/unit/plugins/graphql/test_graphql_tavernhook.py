6a73608d6529fd7562b549a416c3c124e60adc6e
Michael Boulton <michaelboulton@gmail.com>
2025-10-25 15:47:26 +0100

    refactor(tests): Simplify imports in GraphQL Tavern hook tests
    - Replaced `TavernGraphQLPlugin` references with `tavernhook` module imports for better clarity and consistency.

--- tests/unit/plugins/graphql/test_graphql_tavernhook.py
+++ tests/unit/plugins/graphql/test_graphql_tavernhook.py
@@ -1,20 +1,20 @@
 import dataclasses
 
+from tavern._plugins.graphql import tavernhook
 from tavern._plugins.graphql.client import GraphQLClient
-from tavern._plugins.graphql.tavernhook import TavernGraphQLPlugin
 
 
 class TestTavernGraphQLPlugin:
     def test_plugin_properties(self):
-        assert TavernGraphQLPlugin.session_type == GraphQLClient
-        assert TavernGraphQLPlugin.request_block_name == "graphql_request"
-        assert TavernGraphQLPlugin.response_block_name == "graphql_response"
+        assert tavernhook.session_type == GraphQLClient
+        assert tavernhook.request_block_name == "graphql_request"
+        assert tavernhook.response_block_name == "graphql_response"
 
     def test_get_expected_from_request_with_response(self, graphql_test_block_config):
         response_block = {"status_code": 200}
         session = GraphQLClient()
 
-        result = TavernGraphQLPlugin.get_expected_from_request(
+        result = tavernhook.get_expected_from_request(
             response_block, graphql_test_block_config, session
         )
 
@@ -26,7 +26,7 @@ class TestTavernGraphQLPlugin:
         response_block = None
         session = GraphQLClient()
 
-        result = TavernGraphQLPlugin.get_expected_from_request(
+        result = tavernhook.get_expected_from_request(
             response_block, graphql_test_block_config, session
         )
 
@@ -41,7 +41,7 @@ class TestTavernGraphQLPlugin:
 
         session = GraphQLClient()
 
-        result = TavernGraphQLPlugin.get_expected_from_request(
+        result = tavernhook.get_expected_from_request(
             response_block, test_block_config, session
         )
 
