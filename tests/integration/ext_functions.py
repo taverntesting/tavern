093dfd67e481a1cfc5fe8b12a2fbd4250476baa1
Michael Boulton <michaelboulton@gmail.com>
2026-05-31 18:01:57 +0100

    remove nused func

--- tests/integration/ext_functions.py
+++ tests/integration/ext_functions.py
@@ -80,8 +80,3 @@ def global_tincture_marker(stage):
 def get_global_tincture_call_count():
     """Return the number of times global_tincture_marker was called."""
     return {"call_count": sum(_counter.count.values())}
-
-
-def reset_global_tincture_call_count():
-    """Reset the call count (for test isolation)."""
-    _counter.reset()
